ce8a9e0a5a6b33b6eabe27265ed0845dde964433
Danielb2204 <daniel.barreto22400@gmail.com>
2022-12-21 13:36:46 -0300

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,49 +1,96 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
 const productos = [
-    {nombre: "harina", precio: 60},
-    {nombre: "fideos", precio: 80},
-    {nombre: "leche", precio: 40},
-    {nombre: "coca-cola 2L", precio: 160},
-    {nombre: "helado 2L", precio: 250},
+    {id: 1, nombre: "pelota mikasa", precio: 4000, img: "../img/mikasa_piso.jpg"},
+    {id: 2, nombre: "pelota mikasa playa", precio: 2000, img: "../img/mikasa_playa.jpg"},
+    {id: 3, nombre: "pelota molten 4500", precio: 3000, img: "../img/molten_4500.jpg"},
+    {id: 4, nombre: "pelota molten 5000", precio: 3500, img: "../img/molten_5000.jpg"},
+    {id: 5, nombre: "botas mizuno", precio: 9500, img: "../img/botas_mizuno.png"},
+    {id: 6, nombre: "botas asics", precio:10000, img: "../img/botas_asics.jpg"},
+    {id: 7, nombre: "rodilleras mikasa", precio: 1500, img: "../img/rodilleras_mikasa.jpg"},
+    {id: 8, nombre: "rodilleras asics", precio: 1500, img: "../img/rodilleras_asics.jpg"},
 ];
-let carrito = []
+
+let carrito = [];
+
+productos.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="price">${product.precio} $</p>
+    `;
+
+    shopContent.append(content);
+
+    let comprar = document.createElement("button")
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () => {
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+        console.log(carrito);
+    });
+}); 
+
+verCarrito.addEventListener("click", () => {
+    console.log("prueba");
+});
 
 let seleccion = prompt("Hola, desea comprar algún producto? si o no");
 
 while(seleccion != "si" && seleccion != "no"){
-    alert("Por favor, ingresa si o no")
-    seleccion = prompt("Hola, desea comprar algo? si o no")
+    alert("Por favor, ingresa si o no");
+    seleccion = prompt("Hola, desea comprar algo? si o no");
 }
 
 if(seleccion == "si"){
-    alert("A continuación nuestra lista de productos...")
+    alert("A continuación nuestra lista de productos...");
     let todoslosProductos = productos.map(
         (producto) => producto.nombre + " " + producto.precio + "$"
     );
-    alert(todoslosProductos.join(" - "))
+    alert(todoslosProductos.join(" - "));
 } else if(seleccion == "no"){
-    alert("Gracias por visitarnos, hasta pronto!")
+    alert("Gracias por visitarnos, hasta pronto!");
 }
 
 while(seleccion !="no"){
-    let producto = prompt("Agregue un producto al carrito")
-    let precio = 0
+    let producto = prompt("Agregue un producto al carrito");
+    let precio = 0;
 
-    if(producto == "harina" || producto == "fideos" || producto == "fideos" || producto == "leche" || producto == "coca-cola 2L" || producto == "helado 2L"){
+    if(producto == "pelota mikasa" || producto == "pelota mikasa playa" || producto == "pelota molten 4500" || producto == "pelota molten 5000" || producto == "botas mizuno" || producto == "botas asics" || producto == "rodilleras mikasa" || producto == "rodilleras asics"){
         switch (producto){
-            case "harina":
-                precio = 60;
+            case "pelota mikasa":
+                precio = 4000;
                 break;
-            case "fideos":
-                precio = 80;
+            case "pelota mikasa playa":
+                precio = 2000;
                 break;
-            case "leche":
-                precio = 40;
+            case "pelota molten 4500":
+                precio = 3000;
                 break;
-            case "coca-cola 2L":
-                precio = 160;
+            case "pelota molten 5000":
+                precio = 3500;
                 break;
-            case "helado 2L":
-                precio = 250;
+            case "botas mizuno":
+                precio = 9500;
+                break;
+            case "botas asics":
+                precio = 10000;
+                break;
+            case "rodilleras mikasa":
+                precio = 1500;
+                break;
+            case "rodilleras asics":
+                precio = 1500;
                 break;
                 default:
                     break;   
@@ -69,3 +116,58 @@ while(seleccion !="no"){
 
 const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
 console.log(`El total a pagar por su compra es: ${total}`)
+
+//Funcion constructora
+function producto(nombre, precio, img){
+    this.id = productos.length + 1;
+    this.nombre = nombre;
+    this.precio = parseFloat(precio);
+    if(this.img === undefined){
+        this.img = "https://https://via.placeholder.com/300";
+    } else{
+        this.img = img;
+    }
+}
+
+//Funciones de filtrado
+
+console.log(productos);
+
+function findProduct(arr, filtro){
+    const encontrado = arr.find((product)=>{
+        return product.nombre.includes(filtro.toLowerCase())
+    })
+    return encontrado;
+}
+//let buscar = prompt("Ingresa el nombre del producto")
+const prodEncontrado = findProduct(productos, buscar)
+console.log(prodEncontrado);
+
+//Funcion de filtrado generica
+
+function filtrar(arr, filtro, param){
+    return arr.filter((el)=>{
+        if(param == "precio"){
+            return el[param] <= filtro;
+        } else if (param === "id"){
+            return el[param] == filtro;
+        } else{
+            return el[param].includes(filtro);
+        }
+    });
+}
+
+//Filtrar por precio
+const porPrecio = filtrar(productos, 7000, "precio")
+
+console.log(porPrecio);
+
+//Filtrar por ID
+const porId = filtrar(productos, 3, "id")
+
+console.log(porId);
+
+//Filtrar por nombre
+const porNombre = filtrar(productos, "mi", "nombre")
+ console.log(porNombre);
+

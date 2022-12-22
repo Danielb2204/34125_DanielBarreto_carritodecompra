f2cdc8c830bde7a369e6cfa559f54a5516aa3a5e
Danielb2204 <daniel.barreto22400@gmail.com>
2022-12-22 02:02:53 -0300

    prueba arreaglando rutas

--- js/main.js
+++ js/main.js
@@ -83,6 +83,7 @@ verCarrito.addEventListener("click", () => {
    modalContainer.append(totalBuying);
 });
 
+/*
 //Funcion constructora
 function producto(nombre, precio, img){
     this.id = productos.length + 1;
@@ -137,3 +138,4 @@ console.log(porId);
 const porNombre = filtrar(productos, "mi", "nombre")
  console.log(porNombre);
 
+*/
